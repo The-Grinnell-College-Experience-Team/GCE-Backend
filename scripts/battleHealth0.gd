339c199e2f3c7f3aaf99ee77843c71ff09233023
JasperGray <grayjasp@grinnell.edu>
2024-04-08 11:27:54 -0500

    added sexiness (flirt button)

--- scripts/battleHealth0.gd
+++ scripts/battleHealth0.gd
@@ -8,8 +8,12 @@ var max_player_health = 500
 var current_player_health  = 500
 
 var player_strength = 75
-var enemy_strength = 45
+var current_player_strength = 75
+var enemy_strength = 100
+var current_enemy_strength = 150
 
+var player_hotness = 100
+var enemy_hotness = 20
 
 # set up battle setting constants
 var timewait = 0.75
@@ -38,11 +42,26 @@ func set_enemy_health(new_health):
 	$EnemyHealth.value = current_enemy_health
 	pass
 	
+# a simple function to update the enemy health bar to the set value
+# input: an integer for the updated health
+# output: void
+func set_enemy_strength(new_strength):
+	current_enemy_strength = new_strength
+	pass
+	
 # a simple function to subtract the enemy health bar by the set value
 # input: an integer for the difference in health
 # output: void
 func subtract_enemy_health(difference):
 	set_enemy_health(current_enemy_health - difference)
+	pass
+
+# a simple function to subtract the enemy health bar by the set value
+# input: an integer for the difference in health
+# output: void
+func subtract_enemy_strength(difference):
+	if (current_enemy_strength - difference > 0):
+		set_enemy_strength(current_enemy_strength - difference)
 	pass
 
 # a simple function to update the player health bar to the set value
@@ -71,7 +90,7 @@ signal npc_attack
 # then the function signals for the NPC's turn to play out
 func _on_attack_button_pressed():
 	# attack the enemy
-	subtract_enemy_health(player_strength)
+	subtract_enemy_health(current_player_strength)
 	
 	# end the game if the enemy loses all health
 	if current_enemy_health <= 0:
@@ -89,10 +108,26 @@ func _on_attack_button_pressed():
 	
 	pass 
 
+# a function to subtract the enemy strength by the player's strength upon pressing the attack button
+# then the function signals for the NPC's turn to play out
+func _on_flirt_button_pressed():
+	# attack the enemy
+	subtract_enemy_strength(player_hotness)
+	
+	# disable the attack button until the enemy has attacked
+	$Panel/AttackButton.disabled = true
+	
+	# if the battle hasn't finished, then we signal the NPC's attack
+	if not finishedBattle:
+		await get_tree().create_timer(timewait).timeout
+		npc_attack.emit()
+	
+	pass 
+
 # a function to subtract the player health by the enemy's strength upon getting the signal for it
 func _on_npc_attack():
 	# attack the player
-	subtract_player_health(enemy_strength)
+	subtract_player_health(current_enemy_strength)
 	
 	# end the game if the player loses all health
 	if current_player_health <= 0:
